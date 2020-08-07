8174f792920242bdf4c8fb04e6d5d721d9b84533
Steven Gerritsen <steven.gerritsen@kadaster.nl>
2020-08-07 12:44:08 +0200

    Add boostrap file

---
+++ bash_functions.d/shell.sh
@@ -0,0 +1,32 @@
+# Check if this is Windows.
+isWindows() { [[ -n "$WINDIR" ]]; }
+
+# Cross-platform symlink function. It needs two parameters.
+# It will create a symlink to a file or directory, with syntax:
+# link $sourceLocation $symlinkLocation
+link() {
+    source=$1
+    symlink=$2
+    if isWindows; then
+
+        windowsSymlink=${symlink//\//\\}
+        windowsSymlink=${windowsSymlink//\\c/c\:}
+        windowsSource=${source//\//\\}
+        windowsSource=${windowsSource//\\c/c\:}
+        # Windows needs to be told if it's a directory or not. Infer that.
+        # Also: note that we convert `/` to `\`. In this case it's necessary.
+        if [[ -d "$source" ]]; then
+            cmd <<< "mklink /D \"${windowsSymlink}\" \"${windowsSource}\"" > /dev/null
+        else
+            cmd <<< "mklink \"${windowsSymlink}\" \"${windowsSource}\"" > /dev/null
+        fi
+    else
+        # You know what? I think ln's parameters are backwards.
+        ln -s "$source" "$symlink"
+    fi
+    if [ $? -eq 0 ]; then
+        echo "Created symlink: ${source} -> ${symlink}"
+    else
+        echo "Something went wrong, cannot create symlink: ${source} -> ${symlink}"
+    fi
+}
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DOTFILES_DIR="$(dirname "$0")"
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Make symlink in the home folder to .dotfiles if not exists.
 # This way scripts can keep referring to .dotfiles directory even if this is on another location.
@@ -8,13 +8,15 @@ if [ ! -d "${HOME}/.dotfiles" ]; then
     ln -s DOTFILES_DIR ${HOME}/.dotfiles
 fi
 
+source ${DOTFILES_DIR}/bash_functions.d/shell.sh
+
 processFile() {
     if [ $# -lt 2 ]; then
         echo "Need at least a source file and a destination file"
         exit 1
     fi
     sourceFile=$1
-    destinationFile=$2
+    SymlinkFileLocation=$2
     backupDir="${HOME}/.dotfiles_old"
 
     if [ ! -f $sourceFile ]; then
@@ -28,14 +30,14 @@ processFile() {
     fi
 
     #if an old bashrc file exists make a backup of it
-    if [[ -f ${destinationFile} ]]; then
-        fileToBackup="$destinationFile"
+    if [[ -f ${SymlinkFileLocation} ]]; then
+        fileToBackup="$SymlinkFileLocation"
 
         # If we want to create a local file and it already exists it needs a back-up
         # If it doesn't exists yet, nothing needs a back-up
         if [ ${createLocal} = true ]; then
-            if [ -f "${destinationFile}_local" ]; then
-                fileToBackup="${destinationFile}_local"
+            if [ -f "${SymlinkFileLocation}_local" ]; then
+                fileToBackup="${SymlinkFileLocation}_local"
             else
                 fileToBackup=""
             fi
@@ -43,29 +45,33 @@ processFile() {
 
         if [[ -f ${fileToBackup} ]] && [[ ! -L ${fileToBackup} ]]; then
             # To make sure the backup is unique (in case of doing this more often), add a short sha of the file
-            shaChecksum=sha1sum --quiet ${fileToBackup}
+            shaChecksum=$(sha256sum ${fileToBackup})
             shaChecksum=${shaChecksum:0:10}
 
             # Make a folder as backup location for existing dotfiles.
             if [ ! -d "${backupDir}" ]; then
                 mkdir ${backupDir}
             fi
-            mv -f ${fileToBackup} "${backupDir}/$(basename fileToBackup)_${shaChecksum}_old"
+            mv -f ${fileToBackup} "${backupDir}/$(basename $fileToBackup)_${shaChecksum}_old"
         fi
 
         # If we want to create a local file, then move the old file to file_local
         if [ ${createLocal} = true ]; then
-            mv -f ${destinationFile} "${destinationFile}_local"
+            mv -f ${SymlinkFileLocation} "${SymlinkFileLocation}_local"
+        fi
+
+        # It is a symlink just remove it.
+        if [ -L ${SymlinkFileLocation} ]; then
+            rm -f ${SymlinkFileLocation}
         fi
     fi
 
     # Create symlink form source to destination
-    ln -s ${sourceFile} ${destinationFile}
-    echo "Created symlink: ${destinationFile} -> ${sourceFile}"
+    link ${sourceFile} ${SymlinkFileLocation}
 }
 
 #processFile ${DOTFILES_DIR}/.bash_profile ${HOME}/.bash_profile true
 #processFile ${DOTFILES_DIR}/.bashrc ${HOME}/.bashrc true
 #processFile ${DOTFILES_DIR}/.gitconfig ${HOME}/.gitconfig
 #processFile ${DOTFILES_DIR}/.minttyrc ${HOME}/.minttyrc
-processFile ${DOTFILES_DIR}/.editorconfig ${HOME}/.editorconfig
+processFile "${DOTFILES_DIR}/.editorconfig" "${HOME}/.editorconfig"
