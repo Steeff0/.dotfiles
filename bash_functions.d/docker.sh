54f5ac73a0e6139f98f0debe7b5ff81f6458b904
Steven Gerritsen <steven.gerritsen@kadaster.nl>
2020-08-10 15:43:25 +0200

    Update docker functions

--- bash_functions.d/docker.sh
+++ bash_functions.d/docker.sh
@@ -1,12 +1,23 @@
-alias d='winpty docker'
-alias dc='docker-compose'
-alias k='kubectrl'
-
 # Userful functions
+function docker-exec {
+    if [ ! $# -ge 2 ]; then
+        echo "The 'docker-exec' command needs a container hash (param 1) and command as parameter(s)"
+    else
+        container="$1"
+        shift
+        winpty docker exec -it "${container}" $@
+    fi
+}
+
 function docker-bash {
     if [ -z "$1" ]; then
         echo "The 'docker-bash' command needs a container name as parameter"
     else
-        winpty docker exec -it "$1" bash
+        docker-exec $1 bash
     fi
 }
+
+alias d='winpty docker'
+alias de='docker-exec'
+alias dc='docker-compose'
+alias k='kubectrl'
